0650fd05222597b9959115366912911e7928cb1c
akillewis1095 <akillewis1095@macbook-air-2.kutztown.edu>
2018-09-07 08:42:33 -0400

    First Commit

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -1,3 +1,15 @@
+/************************************************************/
+/* Author: Akil Lewis                                       */
+/* Major: Information Technology                            */
+/* Creation Date: September 07, 2018                        */
+/* Due Date: September 7, 2018                              */
+/* Course: CSC243 010                                       */
+/* Professor Name: Professor Demarco                        */
+/* Assignment: #1                                           */
+/* Filename: HelloWorld.java                                */
+/* Purpose: This program is just printing basic hello world */
+/* java code                                                */
+/************************************************************/
 
 public class HelloWorld {
 
